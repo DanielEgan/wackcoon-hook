27f71eb94a68234981cf51526f5daaa6f6c141b0
Jeremy Foster <jeremy.foster@live.com>
2016-06-15 19:37:51 -0700

    add reset and clean tasks

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var app = express();
 var exec = require('child_process').exec;
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -21,22 +20,30 @@ app.post('/payload', function (req, res) {
 	//verify that the payload is a push from the correct repo
 	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
 	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
-	console.log('pulling code from GitHub...');
-	exec('git -C ~/projects/wackcoon-device pull -f', function(err, stdout, stderr) {
-		if (stdout) console.log(stdout);
-		if (stderr) console.log(stderr);
-		console.log('done pull');
-	});
 
-	exec('npm -C ~/projects/wackcoon-device install', function(err, stdout, stderr) {
-		if (stdout) console.log(stdout);
-		if (stderr) console.log(stderr);
-		console.log('done npm install');
-	});
-	res.sendStatus(200);
-	res.end();
+	console.log('pulling code from GitHub...');
+
+	// reset any changes that have been made locally
+	exec('git -C ~/projects/wackcoon-device reset --hard', execCallback);
+
+	// and ditch any files that have been added locally too
+	exec('git -C ~/projects/wackcoon-device clean -df', execCallback);
+
+	// now pull down the latest
+	exec('git -C ~/projects/wackcoon-device pull -f', execCallback);
+
+	// and npm install with --production
+	exec('npm -C ~/projects/wackcoon-device install --production', execCallback);
 });
+
+res.sendStatus(200);
+res.end();
 
 app.listen(5000, function () {
 	console.log('listening on port 5000')
 });
+
+function execCallback(err, stdout, stderr) {
+	if(stdout) console.log(stdout);
+	if(stderr) console.log(stderr);
+}
